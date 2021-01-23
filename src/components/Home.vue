c82da87a9304b79074f2d9cbe36664c6b4310155
Thomas Lhoest <tlhoest@gmail.com>
2021-01-23 11:10:43 +0100

    Update Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,8 +8,72 @@
     <!-- embed youtube -->
     
     <div class="cloud left">
-      <p class="big">A musical album and trans-media project</p>
-      <p class="subtitle">by Philippe Cohen Solal & Mike Lindsay</p>
+      <div class="content">
+        <p class="big">A <strong>musical album</strong> and <strong>trans-media project</strong></p>
+        <p class="subtitle">by Philippe Cohen Solal & Mike Lindsay</p>
+      </div>    
+      <img src="@/assets/img/ui/clouds/c0.svg" class="cb c0">
+    </div>
+
+    <div class="space"></div>
+
+    <div class="cloud right">
+      <p class="quote">« This is my story, this <br> is my song. »</p>
+      <p class="signature">- Henry Darger</p>
+    </div>
+
+    <div class="space"></div>
+
+    <div class="cloud left">
+      <p class="quote">« Both majestic and baroque. »</p>
+      <p class="signature">- FIP</p>
+    </div>
+
+    <div class="space"></div>
+
+    <div class="cloud right">
+      <p><strong>OUTSIDER</strong> transports us into Henry Darger's imaginary and "unreal realm" by <strong>mixing music and dreamlike imagery</strong>. Based on animations, this video creation invites the spectator to immerse himself in <strong>the hallucinatory world of this phenomenal artist</strong>.</p>
+    </div>
+
+    <div class="space"></div>
+
+    <!-- album -->
+
+    <div class="space"></div>
+
+    <div class="cloud left">
+      <p class="quote">« I dream that this transmedia project brings together past, present and future. »</p>
+      <br>
+      <p>« I dream that this transmedia project <strong>brings together past, present and future</strong>. Music, visual arts and storytelling. I would like that through this audiovisual and interactive creation inspired by the work and life of the most famous artist of Art Brut, the public questions <strong>the role and relationship of celebrity to the necessity of art</strong>. » <br><br> - Philippe Cohen Solal</p>
+    </div>
+
+    <div class="space"></div>
+
+    <div class="cloud right">
+      <p class="quote">« These are not your average love songs. »</p>
+      <br>
+      <p>In North Side Chicago, in the middle of the 20th century, 
+        there was a reclusive hospital janitor who lived in one room – 
+        and <strong>who turned out to be an artistic genius</strong>. 
+        <strong>Henry Darger invented a whole world inside that room, making 
+        hundreds of paintings and collages, and writing fantasy novels 
+        that were 15,000 pages long</strong>. His work told the story of an ongoing 
+        battle between good and evil, where rampaging armies battled a band 
+        of brave children called The Vivian Girls.</p>
+      <p>Nearly a half century after Darger’s death, <strong>Gotan Project’s Philippe 
+        Cohen Solal and Tunng’s Mike Lindsay present OUTSIDER</strong>, a tapestry of 
+        ten warm and resonant, rapturous pop songs and gorgeous visuals that 
+        transmit and translate Darger’s story into the current day.
+        <strong>These are heart-soaked instant classics, brought to life in remarkable
+         style by singers Adam Glover and Hannah Peel</strong>. These are not your average 
+         love songs.</p>
+    </div>
+
+    <div class="space"></div>
+
+    <div class="cloud left">
+      <p class="quote">« It sounds like a very twisted long-forgotten musical, with an evil twist and a very innocent blanket wrapped around it. It’s classic, it’s beautiful and it’s experimental. »</p>
+      <p class="signature">- Mike Lindsay</p>
     </div>
 
     <div class="space"></div>
