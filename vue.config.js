5e8d0bd692dfb6ae813e911a5a9840f10dd879cc
Thomas Lhoest <tlhoest@gmail.com>
2021-02-23 20:01:04 +0100

    Add sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
 publicPath: process.env.NODE_ENV === 'production'
     ? '/'
-    : '/'
+    : '/',
+    pluginOptions: {
+        sitemap: {
+            urls: [
+                'http://www.outsideronline.co/'
+            ]
+        }
+    }
 }
